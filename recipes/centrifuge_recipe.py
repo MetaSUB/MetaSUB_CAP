3c1b083d2603f5bad3ae882cd3a306c45ab606d2
David Danko <dcdanko@gmail.com>
2018-02-26 10:47:08 -0500

    recipes for centrifuge and gottcha dbs

---
+++ recipes/centrifuge_recipe.py
@@ -0,0 +1,38 @@
+from packagemega import BaseRecipe, SourceFile
+from glob import glob
+from os.path import basename, dirname, join
+
+
+class CentrifugeRecipe(BaseRecipe):
+
+    def __init__(self):
+        super(CentrifugeRecipe, self).__init__()
+        self.nt = SourceFile(self.repo, "nt.1.cf")
+        self.phv = SourceFile(self.repo, "p+h+v.1.cf")
+
+    def name(self):
+        return 'centrifuge'
+
+    def fileTypes(self):
+        return ['centrifuge_index']
+
+    def resultSchema(self):
+        return {
+            'nt': ['centrifuge_index'] * 3,
+            'p_h_v': ['centrifuge_index'] * 3
+        }
+
+    def makeRecipe(self):
+
+        def resolve(name, source):
+            source.resolve()
+            base = basename(self.source.filepath())
+            dirpath = dirname(self.source.filepath())
+            pattern = base.split('.')
+            pattern = pattern[0] + '.*.' + pattern[2]
+            pattern = join(dirpath, pattern)
+            indices = glob(pattern)
+            self.repo.saveFiles(self, name, *indices)
+
+        resolve('nt', self.nt)
+        resolve('p_h_v', self.phv)
