92ea8afdbf327ba21b450f58ecc0d22fe9b0da33
Danko David <dcd3001@hippocampus.pbtech>
2018-04-21 18:45:15 -0400

    fixed quantify_script

--- scripts/quantify_macrobial.py
+++ scripts/quantify_macrobial.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import pysam
 import click
 from json import loads, dumps
 from sys import stdout
@@ -13,8 +14,29 @@ def millions_of_reads(read_stats_file):
 
 
 def reads_per_chrm(bamf):
-    pass
-
+    samfile = pysam.AlignmentFile(bamf, "rb")
+    read_weights = {}
+    raw_chrm_tbl = {}
+    for rec in samfile:
+        try:
+            read_weights[rec.query_name] += 1
+        except KeyError:
+            read_weights[rec.query_name] = 1
+        try:
+            raw_chrm_tbl[rec.reference_name].append(rec.query_name)
+        except KeyError:
+            raw_chrm_tbl[rec.reference_name] = [rec.query_name]
+    samfile.close()
+    chrm_tbl = {}
+    for chrm, reads in raw_chrm_tbl.items():
+        weight = 0
+        for read in reads:
+            weight += 1 / read_weights[read]
+        chrm_tbl[chrm] = weight
+    return chrm_tbl
+    
+            
+    
 
 def agg_reads(chrs, read_tbl):
     tot_reads = 0
@@ -31,10 +53,10 @@ def agg_reads(chrs, read_tbl):
 @click.argument('biases')
 @click.argument('bam')
 def main(read_stats, biases, bam):
-    biases = loads(biases)
+    biases = loads(open(biases).read())
     read_tbl = reads_per_chrm(bam)
     tbl = {}
-    for gname, bias in biases:
+    for gname, bias in biases.items():
         reads = agg_reads(bias['chrs'], read_tbl)
         rpk = reads / (bias['effective_length'] / 1000)
         rpkm = rpk / millions_of_reads(read_stats)
