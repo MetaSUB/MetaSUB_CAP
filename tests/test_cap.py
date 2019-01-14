399471216b12dd3d06afe89000977d54eb731327
David Danko <dcdanko@gmail.com>
2019-01-14 12:55:36 -0500

    added raw read test data

--- tests/test_cap.py
+++ tests/test_cap.py
@@ -5,6 +5,9 @@ from os import chdir
 from subprocess import call
 from functools import wraps
 from tempfile import mkdtemp
+from os.path import dirname
+
+TEST_DIR = dirname(__file__)
 
 
 def in_mu_repo(func):
@@ -21,9 +24,26 @@ def in_mu_repo(func):
     return decorated_function
 
 
+def add_data_to_mu(func):
+    """Run the test in an initialized MU repo."""
+    @wraps(func)
+    def decorated_function(self, *args, **kwargs):
+        call((
+            'datasuper bio add-fastqs '
+            '-1 _1.fastq.gz -2 _2.fastq.gz '
+            'microbiome '
+            f'{TEST_DIR}/sample_data/zymo_control_1.fq.gz '
+            f'{TEST_DIR}/sample_data/zymo_control_2.fq.gz'
+        ))
+        return func(self, *args, **kwargs)
+
+    return decorated_function
+
+
 class TestCAP(TestCase):
     """Test suite for metasub cap."""
 
+    @add_data_to_mu
     @in_mu_repo
     def test_druyrun(self):
         call('moduleultra run -p metasub_cap --dryrun')
